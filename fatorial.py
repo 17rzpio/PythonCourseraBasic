cddb1349ba83dafaa8f21326089745eb81f8888b
17rzpio <74218387+17rzpio@users.noreply.github.com>
2024-07-16 15:31:37 -0300

    Update fatorial.py
    Esse codigo em python é da semana 5 do curso da usp de python e ele calcula o fatorial

--- fatorial.py
+++ fatorial.py
@@ -1,8 +1,22 @@
-# Escreva o seu programa
-def fatorial(n,m):
-    if(m==n):
-        return n
-    return m*(fatorial(n,m-1))
-n=int(input("digite n"))
-m=int(input("digite m"))
-print(fatorial(n,m))
+def fatorial(k):
+    '''(int) -> int
+
+    Recebe um inteiro k e retorna o valor de k!
+
+    Pre-condicao: supoe que k eh um numero inteiro nao negativo.
+    '''
+    if(k==0):
+        return 1
+    if(k == 1):
+        return 1
+
+    # COMPLETE ESSA FUNÇÃO
+
+    return k*(fatorial(k-1))
+
+# testes
+print("0! =", fatorial(0))
+print("1! =", fatorial(1))
+print("5! =", fatorial(5))
+print("17! =", fatorial(17))
+
